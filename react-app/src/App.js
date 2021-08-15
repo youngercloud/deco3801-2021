650bd82b6fad35d6b4b6a85c040fa240a6941e68
v4rd <anoncrazyer@protonmail.com>
2021-08-15 17:44:02 +1000

    recover login page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Home from './Page/HomePage/Home'
+import LoginPage from "./Page/LoginPage/LoginPage";
 class App extends Component {
     render() {
         return (
             <div>
-                <Home/>
+                {/*<Home/>*/}
+                <LoginPage/>
             </div>
         );
     }
