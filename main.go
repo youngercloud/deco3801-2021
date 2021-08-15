c4f9b1853eac2541dbcdef31468ef1871a28b951
Dollar0712 <1416243855@qq.com>
2021-08-15 15:00:48 +1000

    test push

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
 )
 
+type UserInfo struct {
+	ID uint
+	BookingName string
+}
+
 func main()  {
+	dsn := "stu:deco3801@tcp(34.116.85.107:3306)/booking?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&UserInfo{})
+	if err != nil {
+		return
+	}
+
+	u1 := UserInfo{3, "预约1"}
+	u2 := UserInfo{4, "预约2"}
+	// 创建记录
+	db.Create(&u1)
+	db.Create(&u2)
+
+	// 查询
+	var u = new(UserInfo)
+	db.First(u)
+	fmt.Printf("%#v\n", u)
+
 	router:= gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./views", true)))
 	api := router.Group("./api")
@@ -23,6 +53,8 @@ func main()  {
 	router.NoRoute(NoResponse)
 
 	router.Run(":8081")
+
+	//-------------------------------------------
 }
 
 func NoResponse(c *gin.Context) {
@@ -51,7 +83,7 @@ var pa = []Patient {
 	{3, "ZhangJiuling"},
 }
 
-
+//asdasdadasd
 func SpecHandler(c *gin.Context)  {
 	c.Header("Content-Type", "application/json")
 	pName := c.Param("patientName")
