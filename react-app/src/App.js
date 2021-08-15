04ef8a5c4ec5524b0e7aec5a6ddb0381700e2da4
WeijiaTang <weijia.tang@uqconnect.edu.au>
2021-08-15 15:09:43 +1000

    login ui

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import Home from './Page/HomePage/Home'
+import LoginPage from "./Page/LoginPage/LoginPage";
 class App extends Component {
     render() {
         return (
             <div>
-                <Home/>
+                <LoginPage/>
             </div>
         );
     }
