86d6beb63213a45184fcf8ba41c07db8ed21b8d6
phantom <phantom@localhost.localdomain>
2021-08-26 10:52:39 +1000

    add current main

---
+++ react-app/src/Page/MainPage/Language.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './static/language.css'
+import {Button, Col, Row, Select, Space} from "antd";
+import {Option} from "antd/es/mentions";
+import {LeftOutlined, RightOutlined} from "@ant-design/icons";
+
+
+class Language extends Component {
+
+    bookingGoBack(e) {
+        this.props.changeDisplayBack(e)
+    }
+
+    bookingGoNext(e) {
+        this.props.changeDisplayNext(e)
+    }
+
+    render() {
+        const GP_LIST =["Česky", "Dansk", "Deutsch", "English", "Español", "Ελληνική", "Français", "Italiano",
+            "Nederlands", "Русский", "简体中文", "繁體中文", "한국어", "日本語"];
+        const info = GP_LIST.map((d) => <Option value={d} key={d}>{d}</Option>);
+        return (
+            <div className="language-selection">
+                <Row className="language-selection-title">
+                    <Col span={24}>
+                        <span >Which Language you expect your doctor to speak?</span>
+                    </Col>
+                </Row>
+
+                <Row className="language-selection-body">
+                    <Col span={24}>
+                        <Select
+                            className="language-selection-search"
+                            showSearch
+                            style={{ width: 300, textAlign: "center"}}
+                            placeholder="Select a Language"
+                            optionFilterProp="children"
+                            filterOption={(input, option) =>
+                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                            }>
+                            {info}
+                        </Select>
+                    </Col>
+                </Row>
+
+                <Row className="booking-process-button">
+                    <Space size="middle">
+                        <Button onClick={() => {this.bookingGoBack(this.constructor.name)}} type="primary" shape="round" icon={<LeftOutlined style={{position: "relative",
+                            top: "3px"}} />}>
+                            Back
+                        </Button>
+                        <Button onClick={() => {this.bookingGoNext(this.constructor.name)}} type="primary" shape="round" icon={<RightOutlined style={{position: "relative",
+                            top: "3px"}}/>}>
+                            Next
+                        </Button>
+                    </Space>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default Language;
