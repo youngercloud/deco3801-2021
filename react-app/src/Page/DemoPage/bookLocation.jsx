acd6175e005e92b30262cb2d4ca6a3ed72b40bd7
WeijiaTang <twjjwttwj@gmail.com>
2021-09-22 19:37:11 +1000

    cards

--- react-app/src/Page/DemoPage/bookLocation.jsx
+++ react-app/src/Page/DemoPage/bookLocation.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import "./static/bookLocation.css";
-import {Input, Select, Space, Cascader, Button} from 'antd';
+import {Input, Select, Space, Cascader, Button, Col, Row} from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 import {createFromIconfontCN, SettingOutlined} from '@ant-design/icons';
 import {Option} from "antd/es/mentions";
+
 
 
 const distanceOptions=[
@@ -16,6 +18,8 @@ const languageOptions = [
     { value: 'Chinese', label: 'Chinese' },
     { value: 'Japanese', label: 'Japanese' },
 ];
+
+const { Meta } = Card;
 
 export default class Footer extends Component {
     state={
@@ -45,28 +49,74 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="location">
-                <Input.Group compact>
-                    <Input size="large" style={{ width: '50%' }} placeholder="Type Clinic name or Post Code" name="inputGP"
-                           onChange={this.handleGetInputValue} />
-                    <Select size="large" placeholder="Distance" style={{ width: 120 }} value={this.state.distanceSelect}
-                            onChange={this.handleDistanceValue} options={distanceOptions}>
-                        {/*<Option value="1km">1km</Option>*/}
-                        {/*<Option value="1km-3km">1km-3km</Option>*/}
-                    </Select>
+                <div className="inputData">
+                    <Input.Group compact>
+                        <Input size="large" style={{ width: '50%' }} placeholder="Type Clinic name or Post Code" name="inputGP"
+                               onChange={this.handleGetInputValue} />
+                        <Select size="large" placeholder="Distance" style={{ width: 120 }} value={this.state.distanceSelect}
+                                onChange={this.handleDistanceValue} options={distanceOptions}>
+                            {/*<Option value="1km">1km</Option>*/}
+                            {/*<Option value="1km-3km">1km-3km</Option>*/}
+                        </Select>
 
-                    <Select size="large" placeholder="Language" style={{ width: 120 }} value={this.state.languageSelect}
-                            onChange={this.handleLanguageValue} options={languageOptions}>
-                        {/*<Option value="English">English</Option>*/}
-                        {/*<Option value="Chinese">Chinese</Option>*/}
-                        {/*<Option value="Japanese">Japanese</Option>*/}
-                    </Select>
-                    <Button type="primary" icon={<SearchOutlined style={{fontSize:20,paddingLeft:8}}/> }size="large" />
-                </Input.Group>
+                        <Select size="large" placeholder="Language" style={{ width: 120 }} value={this.state.languageSelect}
+                                onChange={this.handleLanguageValue} options={languageOptions}>
+                            {/*<Option value="English">English</Option>*/}
+                            {/*<Option value="Chinese">Chinese</Option>*/}
+                            {/*<Option value="Japanese">Japanese</Option>*/}
+                        </Select>
+                        <Button type="primary" icon={<SearchOutlined style={{fontSize:20,paddingLeft:8}}/> }size="large" />
+                    </Input.Group>
+                </div>
 
-                <p>Input: {this.state.inputGP}</p>
-                <p>Select1: {this.state.distanceSelect}</p>
-                <p>Select2: {this.state.languageSelect}</p>
+                {/*<p>Input: {this.state.inputGP}</p>*/}
+                {/*<p>Select1: {this.state.distanceSelect}</p>*/}
+                {/*<p>Select2: {this.state.languageSelect}</p>*/}
+                <div className="cards">
+                    <Row gutter={[48, 48]} justify="center">
+                        <Col span={9}>
+                            <Card >
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:177,float:"left"}}/>
+                                <h1>AAAAA CLINIC</h1>
+                                <h2>distances:</h2>
+                                <h2>language:</h2>
+                                <Button>$65 - Consultation</Button>
+                            </Card>
+                        </Col>
+                        <Col span={9}>
+                            <Card >
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:177,float:"left"}}/>
+                                <h1>AAAAA CLINIC</h1>
+                                <h2>distances:</h2>
+                                <h2>language:</h2>
+                                <Button>$65 - Consultation</Button>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row gutter={[48, 48]} justify="center">
+                        <Col span={9}>
+                            <Card >
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:177,float:"left"}}/>
+                                <h1>AAAAA CLINIC</h1>
+                                <h2>distances:</h2>
+                                <h2>language:</h2>
+                                <Button>$65 - Consultation</Button>
+                            </Card>
+                        </Col>
+                        <Col span={9}>
+                            <Card >
+                                <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:177,float:"left"}}/>
+                                <h1>AAAAA CLINIC</h1>
+                                <h2>distances:</h2>
+                                <h2>language:</h2>
+                                <Button>$65 - Consultation</Button>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+
             </div>
+
         );
     }
 }
