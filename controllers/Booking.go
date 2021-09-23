b720f4ac1e0ad5be8f5c89752b213ebafcff647b
Han Li <1157798070@qq.com>
2021-09-23 18:30:14 +0800

    han li

--- controllers/Booking.go
+++ controllers/Booking.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	_ "database/sql"
 	"deco3801/models"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-playground/validator/v10/translations/en"
 )
 
 func Booking(c *gin.Context)  {
@@ -23,24 +25,48 @@ func Booking(c *gin.Context)  {
 	})
 }
 
-func bookSearch(c *gin.Context)  {
-	var db = models.InitDB()
-	var clinicInfo models.Institution
-	err := c.Bind(&clinicInfo)
-	if err != nil {
-		return
-	}
+//func calDistance(cunX int, cunY int, gpX int, gpY int) int {
+//	var distance = math.Cbrt(float64((cunX - gpX)(cunX-gpX) + (cunY - gpY)(cunY-gpY)))
+//	return int(distance)
+//}
 
-	if err := db.Create(&clinicInfo).Error; err != nil {
-		c.JSON(200, gin.H{
-			"creation": "false",
-		})
-		return
-	}
-	c.JSON(200, gin.H{
-		"creation": "true",
-	})
-}
+
+//func bookSearch(c *gin.Context)  {
+//	var db = models.InitDB()
+//	var clinicInfo models.Institution
+//	var myLocationX = 0
+//	var myLocationY = 0
+//	//var distance = 0
+//	var input =  ""
+//	var language = ""
+//	err := c.Bind(&clinicInfo)
+//	if err != nil {
+//		return
+//	}
+//
+//	if err := db.Create(&clinicInfo).Error; err != nil {
+//		c.JSON(200, gin.H{
+//			"creation": "false",
+//		})
+//		return
+//	}
+//	c.JSON(200, gin.H{
+//		"creation": "true",
+//	})
+//
+//	GpInformation := models.HospitalGp{}
+//	if unicode.IsNumber(input) {
+//		db.Raw("SELECT A.GpName A.LocationX A.LocationY FROM HostipalGp A, Doctor B WHERE A.id = B. ClinicOrHospital AND B.Language = ? AND PostCode = ?", language, input).Find(&GpInformation)
+//	} else {
+//		db.Raw("SELECT A.GpName A.LocationX A.LocationY FROM HostipalGp A, Doctor B WHERE A.id = B. ClinicOrHospital AND B.Language = ? AND GpName Like = ?", language, "%"+input+"%").Find(&GpInformation)
+//	}
+//
+//	var RealDistance = calDistance(myLocationX,myLocationY, GpInformation.LocationX,GpInformation.LocationY)
+//
+//
+//}
+
+//db.Joins("join Doctor as a on a.ClinicOrHospital = GP.ID").Where("A.Language Like ?", "%"+language+"%").Find(GpInformation)
 
 //func findLanguages(clinicName string) []string {
 //
--- models/models.go
+++ models/models.go
@@ -33,7 +33,7 @@ type Doctor struct {
 	Gender string `gorm:"not null;size:256"`
 	Email string `gorm:"not null;size:256"`
 	PhoneNumber int `gorm:"size:256"`
-	ClinicOrHospital string `gorm:"size:256"`
+	ClinicOrHospital int `gorm:"size:256"`
 	Specialty string `gorm:"size:256"`
 	Language string `gorm:"size:256"`
 }
@@ -57,13 +57,25 @@ type Booking struct {
 	AcceptOrNot bool
 }
 
+type HospitalGp struct {
+	BaseModel
+	GpName string `gorm:"not null;size:256"`
+	PostCode string `gorm:"not null;size:256"`
+	LocationX int `gorm:"not null;size:256"`
+	LocationY int `gorm:"not null;size:256"`
+	Address string `gorm:"not null;size:256"`
+	About string `gorm:"not null;size:256"`
+	OpeningTime string `gorm:"not null;size:256"`
+	Strengths string `gorm:"not null;size:256"`
+}
+
 func InitDB() *gorm.DB {
 	dsn := "stu:deco3801@tcp(34.87.198.176:3306)/users?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		println(err)
 	}
-	err = db.AutoMigrate(&User{}, &Institution{}, &Doctor{}, &Booking{})
+	err = db.AutoMigrate(&User{}, &Doctor{}, &Booking{}, &HospitalGp{})
 	return db
 }
 
