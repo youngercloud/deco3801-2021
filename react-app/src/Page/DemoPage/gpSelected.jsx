2217344fee6f856f1b63352587da1f326296c58b
TANGweijia0517 <twjjwttwj@gmail.com>
2021-09-23 18:46:17 +1000

    tang

---
+++ react-app/src/Page/DemoPage/gpSelected.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import "./static/gpSelected.css";
+
+export default class gpSelected extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ddd</h2>
+            </div>
+        )}
+}
