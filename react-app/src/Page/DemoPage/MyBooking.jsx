4cc356fce1ce256a12941763440d37aace424036
Dollar0712 <1416243855@qq.com>
2021-10-08 22:22:58 +1000

    Done chatting format

--- react-app/src/Page/DemoPage/MyBooking.jsx
+++ react-app/src/Page/DemoPage/MyBooking.jsx
@@ -2,21 +2,72 @@ import React, {Component} from 'react';
 // RCE CSS
 import 'react-chat-elements/dist/main.css';
 // MessageBox component
-import {Input, MessageList, MessageBox} from 'react-chat-elements'
-import {Button, Card, Col, Row, Select, Space} from "antd";
+import {MessageList} from 'react-chat-elements'
+
+import {Button, Card, Col, Input as AntdInput, Row, Select, Space} from "antd";
 import './static/arginote.css'
 import goodman from "../../Images/goodman.jpeg";
 import {SendOutlined} from "@ant-design/icons";
 import {Option} from "antd/es/mentions";
 
+let key = require('../../privateData.json');
+
+const googleTranslate = require("google-translate")(key[0].keyTranslate);
 
 const {Meta} = Card;
 
 class ChatArea extends Component {
+
+    constructor() {
+        super();
+        this.inputReference = React.createRef();
+    }
+
+    state = {
+        languageCodes: [],
+        input: "",
+    }
+
+    componentDidMount() {
+        // load all of the language options from Google Translate to your app state
+
+        googleTranslate.getSupportedLanguages("en", function (err, languageCodes) {
+            getLanguageCodes(languageCodes); // use a callback function to setState
+        });
+
+        const getLanguageCodes = languageCodes => {
+            this.setState({languageCodes});
+        };
+    }
+
+    translateInput = language => {
+        let {input} = this.state;
+        let transInput = "";
+
+        const translating = transInput => {
+            if (input !== transInput) {
+                this.setState({input: transInput});
+                this.forceUpdate()
+            }
+        };
+
+        googleTranslate.translate(input, language, function (err, translation) {
+            transInput = translation.translatedText;
+            translating(transInput);
+        });
+        this.setState({language});
+    };
+
     render() {
-        const GP_LIST =["Česky", "Dansk", "Deutsch", "English", "Español", "Ελληνική", "Français", "Italiano",
+        const {languageCodes, language} = this.state;
+        const GP_LIST = ["Česky", "Dansk", "Deutsch", "English", "Español", "Ελληνική", "Français", "Italiano",
             "Nederlands", "Русский", "简体中文", "繁體中文", "한국어", "日本語"];
-        const info = GP_LIST.map((d) => <Option value={d} key={d}>{d}</Option>);
+        const info = GP_LIST.map(
+            (d) => <Option value={d} key={d}>{d}</Option>
+        );
+        const orig = languageCodes.map(
+            lang => <Option key={lang.language} value={lang.language}>{lang.name}</Option>
+        )
         return (
             <div className="chat-area">
                 <div className="chat-area-content">
@@ -25,22 +76,22 @@ class ChatArea extends Component {
                         lockable={true}
                         downButton={true}
                         toBottomHeight={'100%'}
-                        dataSource={[ {
-                                position: 'right',
-                                type: 'text',
-                                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                                date: new Date(),
-                            }, {
-                                position: 'right',
-                                type: 'text',
-                                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                                date: new Date(),
-                            }, {
+                        dataSource={[{
                             position: 'right',
                             type: 'text',
                             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                             date: new Date(),
-                            }
+                        }, {
+                            position: 'right',
+                            type: 'text',
+                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+                            date: new Date(),
+                        }, {
+                            position: 'right',
+                            type: 'text',
+                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+                            date: new Date(),
+                        }
                             , {
                                 position: 'right',
                                 type: 'text',
@@ -63,62 +114,71 @@ class ChatArea extends Component {
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }, {
+                            },{
                                 position: 'right',
                                 type: 'text',
                                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                                 date: new Date(),
-                            }]}/>
+                            },
+                            ]}/>
                 </div>
                 <div>
-
                     <Select
                         className="chat-language-select"
                         showSearch
                         placeholder="Select a Language"
                         optionFilterProp="children"
-                        onChange={(v) => {
-                            this.setState({select: v})
+                        value={language}
+                        onChange={(e) => {
+                            this.translateInput(e)
                         }}
                         filterOption={(input, option) =>
                             option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
                         }>
-                        {info}
+                        {orig}
                     </Select>
-                    <Input
+                    <AntdInput
                         placeholder="Message"
-                        className="chat-input"
-                        rightButtons={
-                            <Button type="primary" icon={<SendOutlined style={{
-                                position: "relative",
-                                left: "10px", top: '5px'
-                            }}/>} size="large">
-                            </Button>}/>
+                        className="chat-input-original"
+                        onChange={(e) => {
+                            this.setState({
+                                "input": e.target.value,
+                            })
+                        }}/>
+                    <AntdInput
+                        value={this.state.input}
+                        disabled={true}
+                        style={{marginTop: 10}}
+                        placeholder="Message"
+                        className="chat-input-translated"/>
+
+                    <Button style={{marginTop: 20}} type="primary" size="large">
+                    Send Translated Message </Button>
                 </div>
             </div>
         )
