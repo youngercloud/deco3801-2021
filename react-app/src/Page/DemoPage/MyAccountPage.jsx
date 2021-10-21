0f61b63f95799beaa65e01de1eca17746b436fc3
TANGweijia0517 <twjjwttwj@gmail.com>
2021-10-21 16:02:36 +1000

    rang

--- react-app/src/Page/DemoPage/MyAccountPage.jsx
+++ react-app/src/Page/DemoPage/MyAccountPage.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "./static/myAccount.css";
-import {Button, Card, Col, Row} from 'antd';
+import {Button, Card, Col, Image, Row} from 'antd';
+import doctorImage from "../../Images/goodman.jpeg";
 
 const {Meta} = Card;
 
@@ -12,49 +13,61 @@ export default class Footer extends Component {
 
     render() {
         return (
-            <div className="location">
-                <div className="cards">
+            <div className="account">
+
                     <Row gutter={[48, 48]} justify="center">
-                        <Col span={20}>
-                            <Card>
-                                <Row gutter={[48, 48]} justify="center">
-                                    <h1>My information</h1>
-                                </Row>
-                                <div style={{width: '75%', marginLeft: '7.5%', marginBottom: '10%'}}>
-                                    <hr/>
-                                </div>
+                        <h1>My information</h1>
+                    </Row>
+                    <div style={{width: '75%', marginLeft: '12.5%', marginBottom: '5%'}}>
+                        <hr/>
+                    </div>
 
 
-                                <Row gutter={[48, 48]} justify="left">
-                                    <h2>distances:</h2>
-                                    <h2>language:</h2>
-                                    <Col>
-                                        <img alt="example"
-                                             src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                                             style={{width: 177, float: "right", marginRight: '10%'}}/>
-                                        <Button style={{float: "right", marginRight: '10%'}}>
-                                            Edit details
-                                        </Button>
+                    <Row gutter={[48, 48]} justify="left">
+                        <Col span={4}/>
+                        <Col span={10}>
+                            <div style={{textAlign:"left"}}>
+                                <Row>
+                                    <Col span={12}>
+                                        <h2>Name: </h2>
+                                        <h3>{sessionStorage.getItem("name")}</h3>
                                     </Col>
+                                    <Col span={12}>
+                                        <h2>Gender:  </h2>
+
+                                    </Col>
+
+                                </Row>
+                                <Row>
+                                    <Col span={12}>
+                                        <h2>Language: </h2>
+
+                                    </Col>
+                                    <Col span={12}>
+                                        <h2>Date of Birth: </h2>
+
+                                    </Col>
+
+                                </Row>
+                                <Row>
+                                    <h2>Email Address: </h2>
+                                </Row>
+                                <Row>
+                                    <h2>Phone Number: </h2>
                                 </Row>
 
 
-                                <Row gutter={[48, 48]} justify="left">
-                                    <h2>distances:</h2>
-                                    <h2>language:</h2>
-                                </Row>
-
-                                <Row gutter={[48, 48]} justify="left">
-                                    <h2>distances:</h2>
-                                    <h2>language:</h2>
-                                </Row>
-                                <div style={{width: '75%', marginLeft: '7.5%', marginTop: '20%'}}>
-                                    <hr/>
-                                </div>
-                            </Card>
+                            </div>
+                        </Col>
+                        <Col span={6}>
+                            <Image src={doctorImage}/>
                         </Col>
                     </Row>
-                </div>
+
+                    <div style={{width: '75%', marginLeft: '12.5%', marginTop: '10%', marginBottom:"10%",paddingBottom:"10%"}}>
+                        <hr/>
+                    </div>
+
             </div>
 
         );
