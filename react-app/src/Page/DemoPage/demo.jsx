86147fa4fb57542072bd4c7fd3b7bd2d50901098
Dollar0712 <1416243855@qq.com>
2021-10-21 17:06:59 +1000

    fix sidebar

--- react-app/src/Page/DemoPage/demo.jsx
+++ react-app/src/Page/DemoPage/demo.jsx
@@ -153,22 +153,14 @@ class demo extends Component {
                                 <Menu.Item key="3" icon={<MyIcon type="icon-account" style={{fontSize: 28}}/>}
                                            onClick={() => this.handleClick("3")}>
                                     {arr[3]}
-                                    <Button style={{marginBottom: 20, marginLeft: 20}} onClick={() => this.logout()}>
-                                        {sessionStorage.getItem("name") === null ? "login in" : "logout"}
-                                    </Button>
+
                                 </Menu.Item>
-
+                                <Button className="sider-logout-button" onClick={() => this.logout()}>
+                                    {sessionStorage.getItem("name") === null ? "Sign in" :
+                                        <span>Logout</span>}
+                                </Button>
                             </Menu>
-                            <select className="select-language" value={language} onChange={(e) => {
-                                this.changeHandler(e.target.value)
-                            }}>
 
-                                {languageCodes.map(lang => (
-                                    <option key={lang.language} value={lang.language}>
-                                        {lang.name}
-                                    </option>
-                                ))}
-                            </select>
                         </Affix>
                     </Sider>
 
