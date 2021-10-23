7bfbdcfe3969711735dacf1947911db7b505c8f0
Han Li <1157798070@qq.com>
2021-10-23 12:04:59 +0800

    images2

--- controllers/Base.go
+++ controllers/Base.go
@@ -77,22 +77,22 @@ func MainImage(images []models.Image) models.Image {
 func FakeCl() {
 	var db = models.InitDB()
 	var data models.Doctor
-	data.FirstName = "GEETHA "
-	data.LastName = "DHILEEPAN"
-	data.Email = "geeDh@gmail.com"
-	data.PhoneNumber = "0458523533"
+	data.FirstName = "Keyoren"
+	data.LastName = "Fernandes"
+	data.Email = "KeyFer@gmail.com"
+	data.PhoneNumber = "0498800147"
 	data.Password = "1"
-	data.ClinicOrHospital = "MyHealth"
-	data.Specialty = "Women’s health, Child Health, Travel Medicine, Skin Cancer Checks and Surgery, Chronic Medical conditions including Preventative health\n"
-	data.Language = models.TA
-	data.Gender = "Female"
+	data.ClinicOrHospital = "Brisbane GP and Travel Doctor"
+	data.Specialty = "Travel medicine, Diving and underwater medicine"
+	data.Language = models.EN
+	data.Gender = "Male"
 	data.Monday = "9:00-13:00"
-	data.Tuesday = "9:00-14:00"
-	data.Wednesday = "9:00-14:00"
-	data.Thursday = "9:00-12:00"
-	data.Friday = "9:00-12:00"
-	data.Saturday = "9:00-10:00"
-	data.Sunday = "9:00-11:00"
+	data.Tuesday = "9:00-13:00"
+	data.Wednesday = "9:00-13:00"
+	data.Thursday = "9:00-13:00"
+	data.Friday = "9:00-14:00"
+	data.Saturday = "Closed"
+	data.Sunday = "Closed"
 	if err := db.Create(&data).Error; err != nil {
 		fmt.Println("error!")
 	}
@@ -102,38 +102,11 @@ func FakeCl() {
 func FakeImage() {
 	var db = models.InitDB()
 	var data models.Image
-<<<<<<< HEAD
-	data.Name = "MyHealth_NotMain3"
-	data.Path =  "MyHealth_NotMain3.png"
-=======
-	data.Path = ".png"
-	data.Name = "UQ St Lucia Medical Centre.main"
-	data.OwnerName = "UQ St Lucia Medical Centre"
+	data.Name = "Dr"
+	data.Path =  "Dr.png"
 	data.Type = models.GP
+	data.OwnerName = "Dr. Natasha Christa"
 	data.IsMain = true
-
-	if err := db.Create(&data).Error; err != nil {
-		fmt.Println("error!")
-	}
-
-	data.Path = ".png"
-	data.Name = "Toowong Gps.main"
-	data.OwnerName = "Toowong Gps"
-	data.Type = models.GP
-	data.IsMain = true
-
-	if err := db.Create(&data).Error; err != nil {
-		fmt.Println("error!")
-	}
-
-	data.Path = ".png"
-	data.Name = "MyHealth.main"
-	data.OwnerName = "MyHealth"
->>>>>>> d1ef43d8b5bcb0186634e850b54d17762f2d4170
-	data.Type = models.GP
-	data.OwnerName = "MyHealth"
-	data.IsMain = false
-
 	if err := db.Create(&data).Error; err != nil {
 		fmt.Println("error!")
 	}
@@ -144,24 +117,25 @@ func FakeImage() {
 func FakeGp() {
 	var db = models.InitDB()
 	var data models.HospitalGp
-	data.GpName = "MyHealth"
-	data.PostCode = "4066"
-	data.LocationX = -27.483970183572385
-	data.LocationY = 152.9925428430241
-	data.Address = "Shop 17, Level 1 Toowong Village 9 Sherwood Road Toowong QLD 4066"
-	data.About = "Myhealth Toowong is a mixed billing clinic which is conveniently situated on Level 1 (the Kmart Level) in Toowong Village Shopping Centre.\nOur doctors strive to provide utmost quality care to their patients. We welcome new patients and also accommodate walk-in patients where possible. We are RACGP 4th edition standard accredited.\nWe offer a full range of medical services with pathology and a physiotherapist onsite for added convenience. We also offer Travel Vaccines, and are also Yellow Fever and Q Fever accredited.\n"
-	data.Strengths = "full range of medical services%pathology and a physiotherapist"
-	data.Monday = "8:00-17:00"
-	data.Tuesday = "8:00-17:00"
-	data.Wednesday = "8:00-17:00"
-	data.Thursday = "8:00-17:00"
-	data.Friday = "8:00-17:00"
-	data.Saturday = "9:00-15:00"
-	data.Sunday = "9:00-15:00"
+	data.GpName = "Dr. Natasha Christa	"
+	data.PostCode = "4000"
+	data.LocationX = -27.460249000565096
+	data.LocationY = 153.02335695804226
+	data.Address = "448 Boundary St, Spring Hill QLD 4000"
+	data.About = "Welcome to Our GP Medical Centre in Spring Hill, where we have friendly staff and experienced General Practitioners who care for their patients health and well-being.\nWith support of healthcare professionals including nurses, admin and internal/external management sources, our doctors strive to achieve the best for you and your family."
+	data.Strengths = "full range of medical services pathology and a physiotherapist"
+	data.Monday = "8:30-17:00"
+	data.Tuesday = "8:30-17:00"
+	data.Wednesday = "8:30-17:00"
+	data.Thursday = "10:00-19:00"
+	data.Friday = "10:00-13:00"
+	data.Saturday = "Closed"
+	data.Sunday = "Closed"
 	if err := db.Create(&data).Error; err != nil {
 		fmt.Println("error!")
 	}
 }
+
 
 // FakeBooking  Insert fake book information
 func FakeBooking()  {
