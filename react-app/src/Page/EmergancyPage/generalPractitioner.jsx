2de7debed21ee873da649e35f3a476bd1c407659
WeijiaTang <twjjwttwj@gmail.com>
2021-10-23 16:58:00 +1000

    twj

--- react-app/src/Page/EmergancyPage/generalPractitioner.jsx
+++ react-app/src/Page/EmergancyPage/generalPractitioner.jsx
@@ -3,42 +3,44 @@ import logo from "../EmergancyPage/static/gp.png";
 import "./static/emergancyPage1.css";
 
 import cookie from "react-cookies";
+import {Col, Row} from "antd";
 
 let key = require('../../privateData.json');
 const googleTranslate = require("google-translate")(key[0].keyTranslate);
 
 const strings = {
-    0:"General Practitioner",
-    1:"What is GP ?",
-    2:"The general practitioners are doctor who finish the general practice training, the GP have sufficient " +
+    0: "General Practitioner",
+    1: "What is GP ?",
+    2: "The general practitioners are doctor who finish the general practice training, the GP have sufficient " +
         "qualifications to help you deal with health issue. General practitioner is the first people you should " +
         "see when you feel unwell and they will help with all your health need. ",
-    3:"What can GP do for you",
-    4:"Give you health advice",
-    5:"Provide regular medical check-up",
-    6:"Help to manage you and your family health",
-    7:"Help with mental health issues",
-    8:"Treat injuries and illness",
-    9:"Manage long-term condition, such as heart disease, asthma",
-    10:"Prescribe medicines and provide immunisation",
-    11:"Notice",
-    12:"If the GP can not solves your problem, they will refer you to a specialist doctor or hospital, you need a " +
+    3: "What can GP do for you",
+    4: "Give you health advice",
+    5: "Provide regular medical check-up",
+    6: "Help to manage you and your family health",
+    7: "Help with mental health issues",
+    8: "Treat injuries and illness",
+    9: "Manage long-term condition, such as heart disease, asthma",
+    10: "Prescribe medicines and provide immunisation",
+    11: "Notice",
+    12: "If the GP can not solves your problem, they will refer you to a specialist doctor or hospital, you need a " +
         "referral from your GP before you go to see a specialist doctor",
-    13:"Things about hospital you should know",
-    14:"You should go to GP before you go to hospital.",
-    15:"If you have urgent issue such as serious illness and injuries you can go to emergency department of hospital directly.",
-    16:"The public hospital and private hospital have some difference, choose wisely accord to your need.",
-    17:"You can use the online booking function in our website, which can help you book a consult with GP.",
-    18:"Get recommendation from your friend, community or family",
-    19:"After hour service?",
-    20:"If you need medical care during the GP after hour time, you can get advice from helpline.",
-    21:"If under emergency, you can call 000 and use the Emergency Department service.",
-    22:"How to find a GP"
+    13: "Things about hospital you should know",
+    14: "You should go to GP before you go to hospital.",
+    15: "If you have urgent issue such as serious illness and injuries you can go to emergency department of hospital directly.",
+    16: "The public hospital and private hospital have some difference, choose wisely accord to your need.",
+    17: "You can use the online booking function in our website, which can help you book a consult with GP.",
+    18: "Get recommendation from your friend, community or family",
+    19: "After hour service",
+    20: "If you need medical care during the GP after hour time, you can get advice from helpline.",
+    21: "If under emergency, you can call 000 and use the Emergency Department service.",
+    22: "Find a GP"
 
 }
-const arr = [strings["0"], strings["1"], strings["2"], strings["3"], strings["4"], strings["5"], strings["6"],
-    strings["7"], strings["8"], strings["9"], strings["10"], strings["11"], strings["12"], strings["13"], strings["14"]
-    , strings["15"], strings["16"], strings["17"], strings["18"], strings["19"], strings["20"], strings["21"], strings["22"]];
+const arr = []
+for (let k of Object.keys(strings)) {
+    arr.push(strings[k])
+}
 
 
 class generalPractitioner extends Component {
@@ -49,17 +51,17 @@ class generalPractitioner extends Component {
 
     componentDidMount() {
         // load all of the language options from Google Translate to your app state
-        googleTranslate.getSupportedLanguages("en", function(err, languageCodes) {
+        googleTranslate.getSupportedLanguages("en", function (err, languageCodes) {
             getLanguageCodes(languageCodes); // use a callback function to setState
         });
         const getLanguageCodes = languageCodes => {
-            this.setState({ languageCodes });
+            this.setState({languageCodes});
         };
 
         const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             //window.requestAnimationFrame(smoothscroll);
-            window.scrollTo (0,0);
+            window.scrollTo(0, 0);
         }
     }
 
@@ -70,7 +72,7 @@ class generalPractitioner extends Component {
         // translate the question when selecting a different language
         arr.map((value, index) => {
             if (language !== cookieLanguage) {
-                googleTranslate.translate(arr[index], language, function(err, translation) {
+                googleTranslate.translate(arr[index], language, function (err, translation) {
                     console.log(translation.translatedText);
                     transQuestion = translation.translatedText;
                     translating(transQuestion, index);
@@ -86,19 +88,19 @@ class generalPractitioner extends Component {
             }
         };
 
-        this.setState({ language });
-        cookie.save("language", language, { path: "/" });
+        this.setState({language});
+        cookie.save("language", language, {path: "/"});
 
         const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             //window.requestAnimationFrame(smoothscroll);
-            window.scrollTo (0,0);
+            window.scrollTo(0, 0);
         }
     };
 
-    render(){
+    render() {
         const {languageCodes, language} = this.state;
-        return(
+        return (
             <div>
                 <select className="medical-service-select-language" value={language} onChange={(e) => {
                     this.changeHandler(e.target.value)
@@ -112,10 +114,11 @@ class generalPractitioner extends Component {
                 </select>
                 <div className="mainDiv">
                     <h2 className="head1">{arr[0]}</h2>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <img className="imageRight2" src={logo}/>
 
-                    <br/>
-                    <br/>
-                    <br/><img className="imageRight2" src={logo}/>
                     <br/>
                     <br/>
                     <br/>
@@ -123,91 +126,87 @@ class generalPractitioner extends Component {
                         <h2 className="head2">{arr[1]}</h2>
                         <br/>
                         <br/>
-                        <br/><br/>
                         <br/>
                         <br/>
-
-
-                        <p className="fontLeft2">{arr[2]}</p>
+                        <br/>
+                        <div style={{width: "50%"}}>
+                            <p className="fontLeft2">{arr[2]}</p>
+                        </div>
                         <br/>
                     </div>
                 </div>
                 <div className="nextPart3">
-                    <h2 className="head2">{arr[3]}</h2>
+                    <h2 className="head2-second">{arr[3]}</h2>
                     <br/>
                     <br/>
-                    <br/><br/>
                     <br/>
                     <br/>
+                    <br/>
+
                     <p className="fontLeft2">
                         - {arr[4]}
-                        <br/><br/>
+                        <br/>
                         - {arr[5]}
-                        <br/><br/>
+                        <br/>
                         - {arr[6]}
-                        <br/><br/>
+                        <br/>
                         - {arr[7]}
-                        <br/><br/>
+                        <br/>
                         - {arr[8]}
-                        <br/><br/>
+                        <br/>
                         - {arr[9]}...
-                        <br/><br/>
+                        <br/>
                         - {arr[10]}
-                        <br/><br/>
+                        <br/>
+                        <br/>
+                        <div style={{width: "50%", marginLeft: "18%"}}>
+                            <h4 id="redfont">{arr[11]}</h4>
+                            {arr[12]}
+                        </div>
 
-                        <h4 id="redfont">{arr[11]}:</h4>{arr[12]}
-                        <br/><br/>
-
+                        <br/>
+                        <br/>
                     </p>
                 </div>
                 <div className="nextPart">
-                    <h2 className="head2">{arr[13]}</h2>
+                    <h2 className="head2-second">{arr[13]}</h2>
                     <br/>
                     <br/>
                     <br/><br/>
                     <br/>
                     <br/>
-                    <p className="fontLeft2">
-                        - {arr[14]} <br/><br/>
+                    <div>
+                        <p className="fontLeft2">
+                            - {arr[14]}<br/><br/>
 
-                        - {arr[15]}<br/><br/>
+                            - {arr[15]}<br/><br/>
 
-                        - {arr[16]}
-                    </p>
+                            - {arr[16]}
+                        </p>
+                    </div>
+
                     <br/>
                 </div>
-                <div className="nextPart4">
-                    <div className="leftDiv">
+                <Row className="nextPart4">
+                    <Col offset={1} span={10}>
                         <h2 className="head3">{arr[22]}</h2>
-                        <br/>
-                        <br/>
-                        <br/><br/>
-                        <br/>
-                        <br/>
                         <p className="fontLeft2">
                             - {arr[17]}
                             <br/><br/>
                             - {arr[18]}...
                             <br/><br/>
-
                         </p>
-                    </div>
-                    <div className="verticalLine"></div>
-                    <div className="rightDiv">
+                    </Col>
+                    <Col offset={1} span={10}>
                         <h2 className="head3">{arr[19]}</h2>
-                        <br/>
-                        <br/>
-                        <br/><br/>
-                        <br/>
-                        <br/>
                         <p className="fontLeft2">
                             - {arr[20]}
                             <br/><br/>
                             - {arr[21]}
                             <br/><br/>
                         </p>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </div>
         );
     }
--- react-app/src/Page/EmergancyPage/hospitals.jsx
+++ react-app/src/Page/EmergancyPage/hospitals.jsx
@@ -8,31 +8,31 @@ let key = require('../../privateData.json');
 const googleTranslate = require("google-translate")(key[0].keyTranslate);
 
 const strings = {
-    0:"Hospital",
-    1:"Hospitals in Australia",
-    2:"In Australia, there are private hospital and public hospital. When you have urgent health issue, you " +
+    0: "Hospital",
+    1: "Hospitals in Australia",
+    2: "In Australia, there are private hospital and public hospital. When you have urgent health issue, you " +
         "can go to the emergency department of hospitals, if you have normal or general health issue, it is " +
         "recommended that go to see GP first.",
-    3:"What can hospital do for you",
-    4:"Provide surgery service for patient.",
-    5:"Solve the urgent health issue for patient.",
-    6:"Solve the complex health problems for patient, when GP can not help you, hospital will take you and give you treatment.",
-    7:"Things about hospital you should know",
-    8:"You should go to GP before you go to hospital.",
-    9:"If you have urgent issue such as serious illness and injuries you can go to emergency department of hospital directly.",
-    10:"The public hospital and private hospital have some difference, choose wisely accord to your need.",
-    11:"Public Hospital",
-    12:"Patients need to wait before get treatment according to the severity of illness",
-    13:"Public hospital has more comprehensive sections.",
-    14:"The environment of public hospital  may not meet your need(for example, they may not have single people room for patient)",
-    15:"The hospital will arrange doctor for you, you may not choose who be your doctor.",
-    16:"Less cost.",
-    17:"Private Hospital",
-    18:"The private hospital have higher efficiency, normally patient will get treatment as soon as possible",
-    19:"Private hospital may not have sections that you need.",
-    20:"The private hospital can provide better environment and service for patients.",
-    21:"You can choose your doctor, and more personalized",
-    22:"More expensive, if you have insurance, that can help to cover cost"
+    3: "What can hospital do for you",
+    4: "Provide surgery service for patient.",
+    5: "Solve the urgent health issue for patient.",
+    6: "Solve the complex health problems for patient, when GP can not help you, hospital will take you and give you treatment.",
+    7: "Things about hospital you should know",
+    8: "You should go to GP before you go to hospital.",
+    9: "If you have urgent issue such as serious illness and injuries you can go to emergency department of hospital directly.",
+    10: "The public hospital and private hospital have some difference, choose wisely accord to your need.",
+    11: "Public Hospital",
+    12: "Patients need to wait before get treatment according to the severity of illness",
+    13: "Public hospital has more comprehensive sections.",
+    14: "The environment of public hospital  may not meet your need(for example, they may not have single people room for patient)",
+    15: "The hospital will arrange doctor for you, you may not choose who be your doctor.",
+    16: "Less cost.",
+    17: "Private Hospital",
+    18: "The private hospital have higher efficiency, normally patient will get treatment as soon as possible",
+    19: "Private hospital may not have sections that you need.",
+    20: "The private hospital can provide better environment and service for patients.",
+    21: "You can choose your doctor, and more personalized",
+    22: "More expensive, if you have insurance, that can help to cover cost"
 
 }
 const arr = [strings["0"], strings["1"], strings["2"], strings["3"], strings["4"], strings["5"], strings["6"],
@@ -48,17 +48,17 @@ class hospitals extends Component {
 
     componentDidMount() {
         // load all of the language options from Google Translate to your app state
-        googleTranslate.getSupportedLanguages("en", function(err, languageCodes) {
+        googleTranslate.getSupportedLanguages("en", function (err, languageCodes) {
             getLanguageCodes(languageCodes); // use a callback function to setState
         });
         const getLanguageCodes = languageCodes => {
-            this.setState({ languageCodes });
+            this.setState({languageCodes});
         };
 
         const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             //window.requestAnimationFrame(smoothscroll);
-            window.scrollTo (0,0);
+            window.scrollTo(0, 0);
         }
     }
 
@@ -69,7 +69,7 @@ class hospitals extends Component {
         // translate the question when selecting a different language
         arr.map((value, index) => {
             if (language !== cookieLanguage) {
-                googleTranslate.translate(arr[index], language, function(err, translation) {
+                googleTranslate.translate(arr[index], language, function (err, translation) {
                     console.log(translation.translatedText);
                     transQuestion = translation.translatedText;
                     translating(transQuestion, index);
@@ -85,20 +85,20 @@ class hospitals extends Component {
             }
         };
 
-        this.setState({ language });
-        cookie.save("language", language, { path: "/" });
+        this.setState({language});
+        cookie.save("language", language, {path: "/"});
 
         const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
         if (currentScroll > 0) {
             //window.requestAnimationFrame(smoothscroll);
-            window.scrollTo (0,0);
+            window.scrollTo(0, 0);
         }
     };
 
 
-    render(){
+    render() {
         const {languageCodes, language} = this.state;
-        return(
+        return (
             <div>
                 <select className="medical-service-select-language" value={language} onChange={(e) => {
                     this.changeHandler(e.target.value)
@@ -112,7 +112,6 @@ class hospitals extends Component {
                 </select>
                 <div className="mainDiv">
                     <h2 className="head1">{arr[0]}</h2>
-
                     <br/>
                     <br/>
                     <br/><img className="imageRight2" src={logo}/>
@@ -126,9 +125,10 @@ class hospitals extends Component {
                         <br/><br/>
                         <br/>
                         <br/>
+                        <div style={{width: '50%'}}>
+                            <p className="fontLeft2">{arr[2]} </p>
+                        </div>
 
-
-                        <p className="fontLeft2">{arr[2]} </p>
                         <br/>
                     </div>
                 </div>
@@ -139,12 +139,13 @@ class hospitals extends Component {
                     <br/><br/>
                     <br/>
                     <br/>
-                    <p className="fontLeft2">
-                        - {arr[4]}<br/><br/>
-                        - {arr[5]}<br/><br/>
-                        - {arr[6]}
-                        <br/><br/>
-                    </p>
+                    <div style={{width: '70%'}}>
+                        <p className="fontLeft2">
+                            - {arr[4]}<br/><br/>
+                            - {arr[5]}<br/><br/>
+                            - {arr[6]}<br/><br/>
+                        </p>
+                    </div>
                 </div>
                 <div className="nextPart">
                     <h2 className="head2">{arr[7]}</h2>
@@ -153,57 +154,60 @@ class hospitals extends Component {
                     <br/><br/>
                     <br/>
                     <br/>
+                    <div style={{width: '70%'}}>
+                        <p className="fontLeft2">
+                            - {arr[8]} <br/><br/>
+
+                            - {arr[9]}<br/><br/>
+
+                            - {arr[10]}
+                        </p>
+                    </div>
+                    <br/>
+                </div>
+
+                <div className="nextPart">
+                    <h2 className="head2">{arr[11]}</h2>
+                    <br/>
+                    <br/>
+                    <br/><br/>
+                    <br/>
+                    <br/>
                     <p className="fontLeft2">
-                        - {arr[8]} <br/><br/>
-
-                        - {arr[9]}<br/><br/>
-
-                        - {arr[10]}
+                        - {arr[12]}
+                        <br/><br/>
+                        - {arr[13]}
+                        <br/><br/>
+                        - {arr[14]}
+                        <br/><br/>
+                        - {arr[15]}
+                        <br/><br/>
+                        - {arr[16]}
                     </p>
                     <br/>
                 </div>
-                <div className="nextPart2">
-                    <div className="leftDiv">
-                        <h2 className="head3">{arr[11]}</h2>
-                        <br/>
-                        <br/>
+
+                <div className="nextPart">
+                    <h2 className="head2">{arr[17]}</h2>
+                    <br/>
+                    <br/>
+                    <br/><br/>
+                    <br/>
+                    <br/>
+                    <p className="fontLeft2">
+                        - {arr[18]}
                         <br/><br/>
-                        <br/>
-                        <br/>
-                        <p className="fontLeft2">
-                            - {arr[12]}
-                            <br/><br/>
-                            - {arr[13]}
-                            <br/><br/>
-                            - {arr[14]}
-                            <br/><br/>
-                            - {arr[15]}
-                            <br/><br/>
-                            - {arr[16]}
-                        </p>
-                    </div>
-                    <div className="verticalLine"></div>
-                    <div className="rightDiv">
-                        <h2 className="head3">{arr[17]}</h2>
-                        <br/>
-                        <br/>
+                        - {arr[19]}
                         <br/><br/>
-                        <br/>
-                        <br/>
-                        <p className="fontLeft2">
-                            - {arr[18]}
-                            <br/><br/>
-                            - {arr[19]}
-                            <br/><br/>
-                            - {arr[20]}
-                            <br/><br/>
-                            <br/><br/>
-                            - {arr[21]}
-                            <br/><br/>
-                            - {arr[22]}
-                        </p>
-                    </div>
+                        - {arr[20]}
+                        <br/><br/>
+                        - {arr[21]}
+                        <br/><br/>
+                        - {arr[22]}
+                    </p>
+                    <br/>
                 </div>
+
             </div>
         );
     }
